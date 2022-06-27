52adf57b50e65d53c765e63ae6e1473ced4a13b3
solve <solve@DESKTOP-1LQHUNT>
2022-06-27 09:42:49 +0200

    Add project files.

---
+++ GitHubExsample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubExsample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
